51bade98c403406457d83273b4daa0f2aba73af4
Mick F <mick@mickf.net>
2023-03-13 09:19:13 +0100

    docs: ✏️ Add basic documentation about Transport

--- Sources/Blocks/Transport/Transport.swift
+++ Sources/Blocks/Transport/Transport.swift
@@ -2,14 +2,21 @@ import Foundation
 
 @available(iOS 15.0.0, *)
 @available(macOS 12.0, *)
+/// A type that can transport URL requests to a server-like target.
 public protocol Transport {
-    func send(urlRequest: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, HTTPURLResponse)
+    func send(
+      urlRequest: URLRequest,
+      delegate: URLSessionTaskDelegate?
+    ) async throws -> (Data, HTTPURLResponse)
 }
 
 @available(iOS 15.0.0, *)
 @available(macOS 12.0, *)
 extension URLSession: Transport {
-    public func send(urlRequest: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, HTTPURLResponse) {
+    public func send(
+      urlRequest: URLRequest,
+      delegate: URLSessionTaskDelegate?
+    ) async throws -> (Data, HTTPURLResponse) {
         let (data, response) = try await self.data(for: urlRequest, delegate: delegate)
 
         guard let httpResponse = response as? HTTPURLResponse else {
