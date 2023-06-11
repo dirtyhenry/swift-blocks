651e7e0614dbc4050d474df6f9bbfeae0d0e7dc9
Mick F <mick@mickf.net>
2023-06-11 18:50:50 +0200

    feat: 🎸 Add new Blocks App to the project

---
+++ Examples/BlocksApp/BlocksApp/BlocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksAppApp.swift
+//  BlocksApp
+//
+//  Created by Mickaël Floc'hlay on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/BlocksApp/BlocksApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BlocksApp
+//
+//  Created by Mickaël Floc'hlay on 11/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
