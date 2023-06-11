984c48f7ab274483328b729037f06ce2f15f52d1
Mick F <mick@mickf.net>
2023-06-11 20:22:44 +0200

    feat: 🎸 Implement a tentative WatchPairingTask

--- Examples/BlocksApp/BlocksApp/BlocksAppApp.swift
+++ Examples/BlocksApp/BlocksApp/BlocksAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  BlocksAppApp.swift
-//  BlocksApp
-//
-//  Created by Mickaël Floc'hlay on 11/06/2023.
-//
-
 import SwiftUI
 
 @main
 struct BlocksAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(model: WatchState())
         }
     }
 }
--- Examples/BlocksApp/BlocksApp/ContentView.swift
+++ Examples/BlocksApp/BlocksApp/ContentView.swift
@@ -1,26 +1,115 @@
-//
-//  ContentView.swift
-//  BlocksApp
-//
-//  Created by Mickaël Floc'hlay on 11/06/2023.
-//
-
+import Combine
+import os
 import SwiftUI
+import WatchConnectivity
+
+let logger = Logger(subsystem: "net.mickf.BlocksApp", category: "Watch")
+
+class WatchPairingTask: NSObject {
+    struct TimedOutError: Error, Equatable {}
+
+    static var shared = WatchPairingTask()
+
+    override private init() {
+        logger.debug("Creating an instance of WatchPairingTask.")
+        super.init()
+    }
+
+    private var activationContinuation: CheckedContinuation<Bool, Never>?
+
+    func isPaired(
+        timeoutAfter maxDuration: TimeInterval
+    ) async throws -> Bool {
+        logger.debug("Just called isPaired")
+        return try await withThrowingTaskGroup(of: Bool.self) { group in
+            group.addTask {
+                try await self.doWork()
+            }
+
+            group.addTask {
+                try await Task.sleep(nanoseconds: UInt64(maxDuration * 1_000_000_000))
+                try Task.checkCancellation()
+                // We’ve reached the timeout.
+                logger.error("Throwing timeout")
+                throw TimedOutError()
+            }
+
+            // First finished child task wins, cancel the other task.
+            let result = try await group.next()!
+            group.cancelAll()
+            return result
+        }
+    }
+
+    private func doWork() async throws -> Bool {
+        let session = WCSession.default
+        session.delegate = self
+
+        return await withCheckedContinuation { continuation in
+            activationContinuation = continuation
+            session.activate()
+        }
+    }
+}
+
+extension WatchPairingTask: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith _: WCSessionActivationState, error: Error?) {
+        logger.debug("Session activation did complete")
+
+        if let error = error {
+            logger.error("Session activation did complete with error: \(error.localizedDescription, privacy: .public)")
+        }
+
+        activationContinuation?.resume(with: .success(session.isPaired))
+    }
+
+    func sessionDidBecomeInactive(_: WCSession) {
+        // Do nothing
+    }
+
+    func sessionDidDeactivate(_: WCSession) {
+        // Do nothing
+    }
+}
+
+class WatchState: ObservableObject {
+    @Published var isPaired: Bool?
+
+    func start() {
+        Task {
+            do {
+                let value = try await WatchPairingTask.shared.isPaired(timeoutAfter: 1)
+                await MainActor.run {
+                    isPaired = value
+                }
+            } catch {
+                print("WatchState caught an error.")
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject var model: WatchState
+
+    init(model: WatchState) {
+        self.model = model
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Is a watch paired with this iPhone?")
+            Text(model.isPaired?.description ?? "…")
         }
         .padding()
+        .onAppear {
+            model.start()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(model: WatchState())
     }
 }
