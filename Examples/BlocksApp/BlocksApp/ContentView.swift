c5e0d41d87c876aef14158d7aada0020deafe40e
Mick F <mick@mickf.net>
2023-06-12 10:03:29 +0200

    refactor: 💡 Move utility to Blocks and import it in the app

--- Examples/BlocksApp/BlocksApp/ContentView.swift
+++ Examples/BlocksApp/BlocksApp/ContentView.swift
@@ -1,96 +1,28 @@
-import Combine
-import os
+import Blocks
 import SwiftUI
-import WatchConnectivity
-
-let logger = Logger(subsystem: "net.mickf.BlocksApp", category: "Watch")
-
-class WatchPairingTask: NSObject {
-    struct TimedOutError: Error, Equatable {}
-
-    static var shared = WatchPairingTask()
-
-    override private init() {
-        logger.debug("Creating an instance of WatchPairingTask.")
-        super.init()
-    }
-
-    private var activationContinuation: CheckedContinuation<Bool, Never>?
-
-    func isPaired(
-        timeoutAfter maxDuration: TimeInterval
-    ) async throws -> Bool {
-        logger.debug("Just called isPaired")
-        return try await withThrowingTaskGroup(of: Bool.self) { group in
-            group.addTask {
-                try await self.doWork()
-            }
-
-            group.addTask {
-                try await Task.sleep(nanoseconds: UInt64(maxDuration * 1_000_000_000))
-                try Task.checkCancellation()
-                // We’ve reached the timeout.
-                logger.error("Throwing timeout")
-                throw TimedOutError()
-            }
-
-            // First finished child task wins, cancel the other task.
-            let result = try await group.next()!
-            group.cancelAll()
-            return result
-        }
-    }
-
-    private func doWork() async throws -> Bool {
-        let session = WCSession.default
-        session.delegate = self
-
-        return await withTaskCancellationHandler(operation: {
-            await withCheckedContinuation { continuation in
-                    activationContinuation = continuation
-                    session.activate()
-            }
-        }, onCancel: {
-            activationContinuation?.resume(returning: false)
-            activationContinuation = nil
-        })
-    }
-}
-
-extension WatchPairingTask: WCSessionDelegate {
-    func session(_ session: WCSession, activationDidCompleteWith _: WCSessionActivationState, error: Error?) {
-        logger.debug("Session activation did complete")
-
-        if let error = error {
-            logger.error("Session activation did complete with error: \(error.localizedDescription, privacy: .public)")
-        }
-
-        // activationContinuation?.resume(with: .success(session.isPaired))
-    }
-
-    func sessionDidBecomeInactive(_: WCSession) {
-        // Do nothing
-    }
-
-    func sessionDidDeactivate(_: WCSession) {
-        // Do nothing
-    }
-}
 
 class WatchState: ObservableObject {
-    @Published var isPaired: Bool?
+    enum State: String {
+        case pending = "⏳ Pending"
+        case paired = "✔ Paired"
+        case unpaired = "✘ Unpaired"
+        case unknown = "🤷‍♂️ Unknown"
+    }
+
+    @Published var state: State = .pending
 
     func start() {
         Task {
-            do {
-                let value = try await WatchPairingTask.shared.isPaired(timeoutAfter: 3)
-                await MainActor.run {
-                    isPaired = value
-                }
-            } catch {
-                print("WatchState caught an error.")
-                await MainActor.run {
-                    isPaired = false
+            let value = try? await UIDevice.current.isPairedWithWatch(timeoutAfter: 3)
+
+            await MainActor.run {
+                switch value {
+                case .none:
+                    state = .unknown
+                case .some(true):
+                    state = .paired
+                case .some(false):
+                    state = .unpaired
                 }
             }
         }
@@ -107,7 +39,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Is a watch paired with this iPhone?")
-            Text(model.isPaired?.description ?? "…")
+            Text(model.state.rawValue)
         }
         .padding()
         .onAppear {
---
+++ Sources/Blocks/Watch/WatchPairingUtil.swift
@@ -0,0 +1,97 @@
+#if os(iOS)
+    import Combine
+    import os
+    import SwiftUI
+    import WatchConnectivity
+
+    @available(iOS 14.0, *)
+    let logger = Logger(subsystem: "net.mickf.Blocks", category: "Watch")
+
+    @available(iOS 14.0, *)
+    /// A utility singleton that can detect if a watch is paired to a device with ease.
+    public class WatchPairingUtil: NSObject {
+        struct TimedOutError: Error, Equatable {}
+
+        static var shared = WatchPairingUtil()
+
+        override private init() {
+            logger.debug("Creating an instance of WatchPairingUtil.")
+            super.init()
+        }
+
+        private var activationContinuation: CheckedContinuation<Bool, Never>?
+
+        public func isPaired(
+            timeoutAfter maxDuration: TimeInterval
+        ) async throws -> Bool {
+            logger.debug("Called isPaired")
+            return try await withThrowingTaskGroup(of: Bool.self) { group in
+                group.addTask {
+                    try await self.activateSession()
+                }
+
+                group.addTask {
+                    try await Task.sleep(nanoseconds: UInt64(maxDuration * 1_000_000_000))
+                    try Task.checkCancellation()
+                    // We’ve reached the timeout.
+                    logger.error("Throwing timeout")
+                    throw TimedOutError()
+                }
+
+                // First finished child task wins, cancel the other task.
+                let result = try await group.next()!
+                group.cancelAll()
+                return result
+            }
+        }
+
+        private func activateSession() async throws -> Bool {
+            let session = WCSession.default
+            session.delegate = self
+
+            // A cancellation handler is required here otherwise, cancellation won't happen and
+            // `withThrowingTaskGroup` will hang.
+            return await withTaskCancellationHandler(operation: {
+                await withCheckedContinuation { continuation in
+                    activationContinuation = continuation
+                    session.activate()
+                }
+            }, onCancel: {
+                // Since the task was cancelled, the value that is returned won't play a role
+                activationContinuation?.resume(returning: false)
+                // But for safety sake, let's remove the reference to prevent another call to `resume`.
+                activationContinuation = nil
+            })
+        }
+    }
+
+    @available(iOS 14.0, *)
+    extension WatchPairingUtil: WCSessionDelegate {
+        public func session(_ session: WCSession, activationDidCompleteWith _: WCSessionActivationState, error: Error?) {
+            logger.debug("Session activation did complete")
+
+            if let error = error {
+                logger.error("Session activation did complete with error: \(error.localizedDescription, privacy: .public)")
+            }
+
+            activationContinuation?.resume(with: .success(session.isPaired))
+        }
+
+        public func sessionDidBecomeInactive(_: WCSession) {
+            // Do nothing
+        }
+
+        public func sessionDidDeactivate(_: WCSession) {
+            // Do nothing
+        }
+    }
+
+    @available(iOS 14.0, *)
+    public extension UIDevice {
+        func isPairedWithWatch(
+            timeoutAfter maxDuration: TimeInterval
+        ) async throws -> Bool {
+            try await WatchPairingUtil.shared.isPaired(timeoutAfter: maxDuration)
+        }
+    }
+#endif
