d2b6026b87d2fde27916881c645ecec6e82ee0c6
Mick F <mick@mickf.net>
2023-07-18 15:37:49 +0200

    feat: 🎸 Bootstrap new example app using TCA
    As in The Composable Architecture, cf.
    https://github.com/pointfreeco/swift-composable-architecture

---
+++ Examples/BlocksAppTCA/BlocksAppTCA/BlocksAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksAppTCAApp.swift
+//  BlocksAppTCA
+//
+//  Created by Mickaël Floc'hlay on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlocksAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
