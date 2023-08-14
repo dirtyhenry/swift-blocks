1eed222b5171cb2a4cb27266cd7f7282ff875d49
Mick F <mick@mickf.net>
2023-08-14 19:17:05 +0200

    feat: 🎸 Add Mac TCA app

---
+++ Examples/BlocksAppMacTCA/BlocksAppMacTCA/BlocksAppMacTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksAppMacTCAApp.swift
+//  BlocksAppMacTCA
+//
+//  Created by Mickaël Floc'hlay on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlocksAppMacTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
