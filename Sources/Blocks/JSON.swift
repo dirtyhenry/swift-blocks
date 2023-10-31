ec9c64bfc297d957cff17b20a8343e09514e5788
Mick F <mick@mickf.net>
2023-10-31 12:25:33 +0100

    fix: 🐛 Small typos and API improvement, docs fixing

--- Sources/Blocks/JSON.swift
+++ Sources/Blocks/JSON.swift
@@ -3,20 +3,20 @@ import Foundation
 /**
    A utility for encoding Codable objects into JSON strings and printing them.
 
-   ## Usage
-
-   Using default are encouraged:
+   ## Basic Usage
 
    ```swift
    let jsonString = JSON.stringify(sampleCodable)
    JSON.print(sampleCodable)
    ```
 
+   In case the stringification fails, a default error output is used. This is an opinionated preference
+   that makes using these functions much more convenient, for instance for debugging and logging in non
+   throwing contexts.
+
    The naming is inspired by JavaScript's [`JSON.stringify()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify).
  */
 public enum JSON {
-    public static let defautErrorOutput = "💥 JSON Encoding did not work"
-
     /**
        Encodes a Codable object into a JSON string.
 
@@ -30,7 +30,7 @@ public enum JSON {
     public static func stringify(
         _ codable: Codable,
         encoder: JSONEncoder = .prettyEncoder(),
-        defaultErrorOutput: String = defautErrorOutput
+        defaultErrorOutput: String = "💥 JSON Encoding did not work"
     ) -> String {
         do {
             let data = try encoder.encode(codable)
@@ -49,12 +49,12 @@ public enum JSON {
        - Parameters:
          - codable: A Codable object to print.
          - encoder: A JSON encoder to use for encoding the object. Default is a pretty-printing encoder.
-         - defaultErrorOutput: A string to return in case of an encoding error (not used in this function).
+         - defaultErrorOutput: A string to return in case of an encoding error. Default is "💥 JSON Encoding did not work."
      */
     public static func print(
         _ codable: Codable,
         encoder: JSONEncoder = .prettyEncoder(),
-        defaultErrorOutput: String = defautErrorOutput
+        defaultErrorOutput: String = "💥 JSON Encoding did not work"
     ) {
         Swift.print(stringify(codable, encoder: encoder, defaultErrorOutput: defaultErrorOutput))
     }
