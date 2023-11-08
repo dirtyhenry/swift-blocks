8c4a013727f62307f567c91332b1a6a6db66711f
Mick F <mick@mickf.net>
2023-11-08 18:14:43 +0100

    fix: 🐛 Repair macOS build

--- Sources/Blocks/Extensions/UIFont.swift
+++ Sources/Blocks/Extensions/UIFont.swift
@@ -1,9 +1,11 @@
-import UIKit
+#if canImport(UIKit)
+    import UIKit
 
-public extension UIFont {
-    static func allFontNames() -> [String] {
-        familyNames.sorted()
-            .map { UIFont.fontNames(forFamilyName: $0).sorted() }
-            .flatMap { $0 }
+    public extension UIFont {
+        static func allFontNames() -> [String] {
+            familyNames.sorted()
+                .map { UIFont.fontNames(forFamilyName: $0).sorted() }
+                .flatMap { $0 }
+        }
     }
-}
+#endif
