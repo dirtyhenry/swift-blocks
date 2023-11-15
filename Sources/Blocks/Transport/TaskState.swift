47d5d9680f1348347aed42e1c5a944e072f8eec6
Mick F <mick@mickf.net>
2023-11-15 12:56:56 +0100

    feat: 🎸 Add TaskStateButton
    BREAKING CHANGE: 🧨 MandatoryTaskState was renamed TaskState.

---
+++ Sources/Blocks/Transport/TaskState.swift
@@ -0,0 +1,24 @@
+public enum TaskState: Equatable {
+    case notStarted
+    // TODO: Add an optional cancellable to the running state?
+    case running
+    case completed
+    case failed(errorDescription: String)
+}
+
+#if DEBUG
+    extension TaskState {
+        func debugLoopNextState() -> TaskState {
+            switch self {
+            case .notStarted:
+                return .running
+            case .running:
+                return .completed
+            case .completed:
+                return .failed(errorDescription: "Dummy error")
+            case .failed:
+                return .notStarted
+            }
+        }
+    }
+#endif
