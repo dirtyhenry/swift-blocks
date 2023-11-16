fa5f5e4bd6a316077875154c11617df0353a5a06
Mick F <mick@mickf.net>
2023-11-16 12:05:17 +0100

    feat: 🎸 Iterate on LoggingTransport
    * Use inbox/outbox emojis
    * Use `public` privacy by default
    * Documentation

--- Sources/Blocks/Transport/LoggingTransport.swift
+++ Sources/Blocks/Transport/LoggingTransport.swift
@@ -1,25 +1,35 @@
 import Foundation
-
 import os
 
 @available(iOS 15.0.0, *)
 @available(macOS 12.0, *)
+/// A transport wrapper that will log network activity to the unified logging system.
+///
+/// This wrapper should be used for **debugging purposes only**, since it will output
+/// both requests and responses in the console with a `public` privacy by default.
 public final class LoggingTransport: Transport {
     let wrapped: Transport
     let logger: Logger
 
-    public init(wrapping: Transport, subsystem: String, category: String = "Networking") {
+    public init(
+        wrapping: Transport,
+        subsystem: String,
+        category: String = "Networking"
+    ) {
         wrapped = wrapping
         logger = Logger(subsystem: subsystem, category: category)
     }
 
-    public func send(urlRequest: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, HTTPURLResponse) {
-        logger.debug("Sending request: \(urlRequest)")
+    public func send(
+        urlRequest: URLRequest,
+        delegate: URLSessionTaskDelegate?
+    ) async throws -> (Data, HTTPURLResponse) {
+        logger.debug("📤 \(urlRequest, privacy: .public)")
         let (data, httpResponse) = try await wrapped.send(urlRequest: urlRequest, delegate: delegate)
         if let dataAsString = String(data: data, encoding: .utf8) {
-            logger.debug("Got response: \(dataAsString) \(httpResponse)")
+            logger.debug("📥 \(dataAsString, privacy: .public) \(httpResponse, privacy: .public)")
         } else {
-            logger.debug("Got response: \(data) \(httpResponse)")
+            logger.debug("📥 \(data, privacy: .public) \(httpResponse, privacy: .public)")
         }
         return (data, httpResponse)
     }
