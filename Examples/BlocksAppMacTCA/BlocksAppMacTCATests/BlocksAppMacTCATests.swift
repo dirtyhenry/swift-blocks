496a527f6d1c87f78ae0e6fb87457014af6bf8ab
Mick F <mick@mickf.net>
2023-11-30 23:29:41 +0100

    feat: 🎸 Add half-backed code from working copy

--- Examples/BlocksAppMacTCA/BlocksAppMacTCATests/BlocksAppMacTCATests.swift
+++ Examples/BlocksAppMacTCA/BlocksAppMacTCATests/BlocksAppMacTCATests.swift
@@ -5,11 +5,10 @@
 //  Created by Mickaël Floc'hlay on 14/08/2023.
 //
 
-import XCTest
 @testable import BlocksAppMacTCA
+import XCTest
 
 final class BlocksAppMacTCATests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class BlocksAppMacTCATests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- Examples/BlocksAppMacTCA/BlocksAppMacTCAUITests/BlocksAppMacTCAUITestsLaunchTests.swift
+++ Examples/BlocksAppMacTCA/BlocksAppMacTCAUITests/BlocksAppMacTCAUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class BlocksAppMacTCAUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
