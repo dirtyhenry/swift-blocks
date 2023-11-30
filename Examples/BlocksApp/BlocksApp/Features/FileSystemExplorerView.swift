9287807f548b4eb092531d4bca44830f33607b71
Mick F <mick@mickf.net>
2023-11-30 22:19:30 +0100

    feat: 🎸 Introduce Objective-C library

---
+++ Examples/BlocksApp/BlocksApp/Features/FileSystemExplorerView.swift
@@ -0,0 +1,6 @@
+import Foundation
+import ObjectiveBlocks
+
+func explore() {
+    FileSystemExplorer.exploreFileSystem()
+}
---
+++ Sources/ObjectiveBlocks/FileSystemExplorer.m
@@ -0,0 +1,48 @@
+#import "FileSystemExplorer.h"
+
+@implementation FileSystemExplorer
+
++ (NSString *)paddingSpaces:(NSUInteger)level {
+    NSMutableString *result = [NSMutableString new];
+    for (int i = 0; i < level; i++) {
+        [result appendString:@"   "];
+    }
+    return result;
+}
+
+
++ (void)exploreDirectory:(NSString *)path level:(NSUInteger)level {
+    NSError *error = nil;
+    NSArray *contents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&error];
+    for (NSString *content in contents) {
+        NSString *fullPath = [path stringByAppendingPathComponent:content];
+
+        NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:fullPath error:&error];
+        if (attributes && [attributes objectForKey:NSFileType] == NSFileTypeSymbolicLink) {
+            NSString *destinationOfLink = [[NSFileManager defaultManager] destinationOfSymbolicLinkAtPath:fullPath error:&error];
+            if (destinationOfLink) {
+                BOOL destinationOfLinkExists = [[NSFileManager defaultManager] fileExistsAtPath:destinationOfLink];
+                NSLog(@"%@|- %@ -%@-> %@", [self paddingSpaces:level], content, destinationOfLinkExists ? @"x" : @"-", destinationOfLink);
+            } else {
+                NSLog(@"ERROR! Is it a symbolic link or not? (error=%@)", error);
+            }
+        } else {
+            BOOL isDirectory = NO;
+            if ([[NSFileManager defaultManager] fileExistsAtPath:fullPath isDirectory:&isDirectory]) {
+                NSLog(@"%@|- %@", [self paddingSpaces:level], content);
+                if (isDirectory) {
+                    [self exploreDirectory:fullPath level:(level + 1)];
+                }
+            }
+        }
+    }
+}
+
+
++ (void)exploreFileSystem {
+    NSString *homeDirectory = NSHomeDirectory();
+    [self exploreDirectory:homeDirectory level:0];
+}
+
+
+@end
