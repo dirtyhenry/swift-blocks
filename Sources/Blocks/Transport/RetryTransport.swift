94274a1b1ba1044b00e1be8133da0d59117dbbe6
Mick F <mick@mickf.net>
2024-04-30 10:14:19 +0200

    feat(transport): add Linux compatibility

--- Sources/Blocks/Transport/RetryTransport.swift
+++ Sources/Blocks/Transport/RetryTransport.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 /// A transport class that retries sending requests based on specified conditions and delays.
 ///
