2ace80f3a2f2de8d40c86673e2748d9909b0a5e0
Mick F <mick@mickf.net>
2024-06-25 20:33:45 +0200

    feat: add scaffold for legacy Martinet app

---
+++ Examples/Martinet/Martinet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Martinet
+//
+//  Created by Mickaël Floc'hlay on 25/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
