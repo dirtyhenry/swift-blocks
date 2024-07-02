7c5213d5e40388892688c8f8c12a1e8c7343782a
Mick F <mick@mickf.net>
2024-07-02 17:12:50 +0200

    feat(extensions): add slugify command to StringProtocol

---
+++ Sources/Blocks/Extensions/StringProtocol.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public extension StringProtocol {
+    /// Converts the string into a URL-friendly slug.
+    ///
+    /// This method transforms the string to lowercase, removes accents and combining marks, replaces spaces and
+    /// non-alphanumeric characters with hyphens, and trims leading and trailing hyphens.
+    ///
+    /// - Returns: A URL-friendly slug representation of the string.
+    ///
+    /// # Usage Example #
+    /// ```
+    /// let exampleString = "Hello, World! This is an example string with accents: é, è, ê, ñ."
+    /// let slugifiedString = exampleString.slugify()
+    /// print(slugifiedString)  // Output: "hello-world-this-is-an-example-string-with-accents-e-e-e-n"
+    /// ```
+    func slugify() -> String {
+        var slug = lowercased()
+
+        // Remove accents and combining marks
+        slug = slug.applyingTransform(.stripDiacritics, reverse: false) ?? slug
+        slug = slug.applyingTransform(.stripCombiningMarks, reverse: false) ?? slug
+
+        // Replace spaces and non-alphanumeric characters with hyphens
+        slug = slug.replacingOccurrences(of: "[^a-z0-9]+", with: "-", options: .regularExpression)
+
+        // Trim hyphens from the start and end
+        slug = slug.trimmingCharacters(in: CharacterSet(charactersIn: "-"))
+
+        if !isEmpty, slug.isEmpty {
+            if let extendedSelf = applyingTransform(.toUnicodeName, reverse: false)?
+                .replacingOccurrences(of: "\\N", with: ""), self != extendedSelf {
+                return extendedSelf.slugify()
+            }
+        }
+
+        return slug
+    }
+}
