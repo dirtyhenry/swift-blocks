fb05728a237842bf9719a1440ea920e812eed905
Mick F <mick@mickf.net>
2024-12-26 10:13:38 +0100

    feat(ui): fix LabeledTextField for macOS

--- Sources/Blocks/UIComponents/LabeledTextField.swift
+++ Sources/Blocks/UIComponents/LabeledTextField.swift
@@ -1,34 +1,49 @@
 import SwiftUI
 
+@available(iOS 15.0, *)
+@available(macOS 12.0, *)
 public struct LabeledTextField: View {
-  private var title: String
-  @Binding var text: String
-  
+    private var title: String
+    @Binding var text: String
+    private var prompt: Text?
+
+    @FocusState private var fieldIsFocused: Bool
+
     public init(
         _ title: String,
-        text: Binding<String>
+        text: Binding<String>,
+        prompt _: Text? = nil
     ) {
-    self.title = title
-    self._text = text
-  }
-  
-  public var body: some View {
-    ZStack(alignment: .leading) {
-      if !text.isEmpty {
-        Text(title)
-          .foregroundColor(text.isEmpty ? Color(.placeholderText) : .accentColor)
-          .offset(y: text.isEmpty ? 0 : -25)
-          .scaleEffect(text.isEmpty ? 1: 0.8, anchor: .leading)
-      }
-      TextField("", text: $text)
+        self.title = title
+        _text = text
     }
-    .padding(.top, 16)
-    .animation(.default, value: text)
-  }
+
+    public var body: some View {
+        #if os(iOS)
+        ZStack(alignment: .leading) {
+            if !text.isEmpty || fieldIsFocused {
+                Text(title)
+                    .foregroundColor(text.isEmpty ? Color(.placeholderText) : .accentColor)
+                    .offset(y: text.isEmpty ? 0 : -25)
+                    .scaleEffect(text.isEmpty ? 1 : 0.8, anchor: .leading)
+            }
+            TextField("", text: $text, prompt: prompt)
+                .focused($fieldIsFocused)
+        }
+        .padding(.top, 16)
+        .animation(.default, value: text)
+        #else
+        TextField(title, text: $text, prompt: prompt)
+        #endif
+    }
 }
 
 #Preview {
     Form {
-        LabeledTextField("Input", text: .constant("Value"))
+        if #available(iOS 15.0, macOS 12.0, *) {
+            LabeledTextField("Input", text: .constant("Value"))
+        } else {
+            Text("macOS 12 is required.")
+        }
     }
 }
